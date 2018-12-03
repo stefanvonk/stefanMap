58e543e817eac9bdaed3a42da3be89a44a3fca59
stefanvonk <s1084294@student.hsleiden.nl>
2018-12-03 15:33:44 +0100

    nmap fix

--- pasive.py
+++ pasive.py
@@ -1,8 +1,7 @@
 import subprocess
-from netaddr import IPNetwork
 
 def pasive():
-    network = "192.168.1.0/24"
+    network = "192.168.11.0/24"
     p = subprocess.Popen(["sudo", "nmap", "-sP", network], stdout=subprocess.PIPE)
 
     for line in p.stdout:
