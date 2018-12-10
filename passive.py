e593350116b9bffcbb30f6b09cb56c5f2257e51b
stefanvonk <s1084294@student.hsleiden.nl>
2018-12-10 12:46:49 +0100

    pyshark fix

--- passive.py
+++ passive.py
@@ -8,7 +8,8 @@ def detectionMethod1(ip):
     # scan network traffic
     logging.info("Start scan network traffic")
 
-    capture = pyshark.LiveCapture().sniff(timeout=30)
+    capture = pyshark.LiveCapture(interface='eth0')
+    capture.sniff(timeout=20)
     for packet in capture.sniff_continuously(packet_count=5):
         print('Just arrived:', packet)
 
@@ -36,7 +37,7 @@ def detectionMethod2(ip):
     except Exception as e:
         logging.warning("The following error raise when trying to get the MAC address from the network machine:" + str(e))
 
-    print("#8: The vendor of the MAC address of this machine is: " + str(vendor))
+    print("#2: The vendor of the MAC address of this machine is: " + str(vendor))
     print("Check manually whether this is virtual machine vendor.")
     logging.info("Result of MAC address vendor: " + str(vendor))
 
